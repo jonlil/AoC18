46924dc9d39cb3f8eb186ddf52295786297afc3c
Jonas Liljestrand <jonas.liljestrand@gmail.com>
2018-12-23 10:04:33 +0100

    dec04 - part 1 solution

--- dec04/src/lib.rs
+++ dec04/src/lib.rs
@@ -4,6 +4,7 @@ use std::fs;
 use regex::Regex;
 use std::str::FromStr;
 use std::collections::HashMap;
+use std::ops::Range;
 
 pub struct Config {
     pub filename: String,
@@ -29,11 +30,11 @@ struct Event {
 
 impl Event {
     fn new(value: &str) -> Result<Event, &'static str> {
-        let event = parse_shift_log(value)?;
+        let event = parse_log_line(value)?;
         Ok(event)
     }
 
-    fn getType(message: &str) -> GuardEvent {
+    fn get_type(message: &str) -> GuardEvent {
         if message.contains("#") {
             GuardEvent::StartsShift
         } else if message.contains("sleep") {
@@ -54,16 +55,57 @@ enum GuardEvent {
 #[derive(Hash, Eq, PartialEq, Debug, Clone)]
 struct Guard {
     id: u32,
+    sleeping_minutes: Vec<i64>,
+    total_sleep: u64,
     events: Vec<Event>,
 }
 
 impl Guard {
     fn new(id: u32) -> Guard {
-        Guard { id: id, events: Vec::new() }
+        Guard {
+            id: id,
+            sleeping_minutes: vec![0i64; 59],
+            total_sleep: 0_u64,
+            events: Vec::new()
+        }
+    }
+
+    fn calculate_sleep(&mut self) {
+        let mut start: u8 = 70;
+        let mut end: u8 = 70;
+
+        for event in &self.events {
+            match event.kind {
+                GuardEvent::FallsAsleep => {
+                    start = event.min.to_owned();
+                },
+                GuardEvent::WakesUp => {
+                    end = event.min.to_owned();
+                    for i in generate_sleep_range(start, end) {
+                        self.sleeping_minutes[i as usize] += 1;
+                    }
+                    self.total_sleep += (end - start) as u64;
+                },
+                GuardEvent::StartsShift => {}
+            }
+
+            //println!("EventType: {:?} EventTime: {}:{}", event.kind, event.hour, event.min);
+        }
+
+        println!("GuardID: {}\n Total sleep: {}",
+            self.id,
+            self.total_sleep);
+        for (key, value) in self.sleeping_minutes.iter().enumerate() {
+            println!("{}: {}", key, value)
+        }
     }
 }
 
-fn parse_shift_log(log: &str) -> Result<Event, &'static str> {
+fn generate_sleep_range(start: u8, end: u8) -> Range<u8> {
+    (start..end).into_iter()
+}
+
+fn parse_log_line(log: &str) -> Result<Event, &'static str> {
     // [1518-11-17 00:22] falls asleep
     let re = Regex::new(r"(?x)
             \[
@@ -91,7 +133,7 @@ fn parse_shift_log(log: &str) -> Result<Event, &'static str> {
         min: FromStr::from_str(&cap["min"]).unwrap(),
         hour: FromStr::from_str(&cap["hour"]).unwrap(),
         message: FromStr::from_str(&cap["message"]).unwrap(),
-        kind: Event::getType(&cap["message"]),
+        kind: Event::get_type(&cap["message"]),
     })
 }
 
@@ -112,7 +154,7 @@ fn parse_guard_from_message(message: &str) -> Result<Guard, &'static str> {
     Ok( Guard::new(FromStr::from_str(&cap[1]).unwrap()) )
 }
 
-pub fn run(config: Config) {
+pub fn run(_config: Config) {
     let contents = fs::read_to_string("seed.sorted")
         .expect("something went wrong reading the file");
 
@@ -120,7 +162,7 @@ pub fn run(config: Config) {
     let mut current_id: u32 = 0;
 
     // TODO: Simplify
-    for event in contents.lines().into_iter().map(parse_shift_log) {
+    for event in contents.lines().into_iter().map(Event::new) {
         let e = event.unwrap();
 
         match e.kind {
@@ -130,7 +172,7 @@ pub fn run(config: Config) {
                         current_id = g.id.to_owned();
 
                         match guards.get_mut(&current_id) {
-                            Some(mut g) => g.events.push(e),
+                            Some(g) => g.events.push(e),
                             None => {
                                 g.events.push(e);
                                 guards.insert(g.id, g);
@@ -144,20 +186,27 @@ pub fn run(config: Config) {
             },
             GuardEvent::FallsAsleep => {
                 match guards.get_mut(&current_id) {
-                    Some(mut g) => g.events.push(e),
+                    Some(g) => g.events.push(e),
                     None => continue
                 }
             },
             GuardEvent::WakesUp => {
                 match guards.get_mut(&current_id) {
-                    Some(mut g) => g.events.push(e),
+                    Some(g) => g.events.push(e),
                     None => continue
                 }
             }
         }
     };
 
-    for (key, guard) in &guards {
-        println!("{:?}", guard);
-    }
+    guards.get_mut(&2953_u32).unwrap().calculate_sleep();
+    // for guard in guards.values_mut() {
+    //     &guard.calculate_sleep();
+    // }
+}
+
+fn sleep_time() {
+    // 50 - 30 = 20
+    // 9 - 0 = 9
+    //end - start
 }
