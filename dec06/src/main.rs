ed82cf6c002d19526561617b88cfe128dfd35094
Jonas Liljestrand <jonas.liljestrand@gmail.com>
2018-12-29 01:47:59 +0100

    convert to i32 instead of usize
    I will use usize for iterating over indexes in memory

--- dec06/src/main.rs
+++ dec06/src/main.rs
@@ -11,24 +11,24 @@ struct Point<T> {
 #[derive(Debug)]
 struct Grid {
     boundry: (
-        Point<usize>,
-        Point<usize>,
+        Point<i32>,
+        Point<i32>,
     )
 }
 
 impl Grid {
-    fn new(b1: Point<usize>, b2: Point<usize>) -> Grid {
+    fn new(b1: Point<i32>, b2: Point<i32>) -> Grid {
         Grid { boundry: (b1, b2) }
     }
 }
 
 impl Iterator for Grid {
-    type Item = Point<usize>;
+    type Item = Point<i32>;
 
     fn next(&mut self) -> Option<Self::Item> {
         Some( Point {
-            x: 0 as usize,
-            y: 0 as usize,
+            x: 0,
+            y: 0,
         } )
     }
 }
@@ -37,7 +37,7 @@ fn read_seed_file() -> String {
     fs::read_to_string("seed").expect("something went wrong reading the file")
 }
 
-fn parse_points(line: &str, index: u8) -> Point<i64> {
+fn parse_points(line: &str, index: u8) -> Point<i32> {
     let re = Regex::new(r"(?P<x>\d+),\s{1}(?P<y>\d+)").unwrap();
 
     match re.captures(line) {
@@ -51,13 +51,13 @@ fn parse_points(line: &str, index: u8) -> Point<i64> {
     }
 }
 
-fn get_points() -> Result<Vec<Point<i64>>, &'static str> {
-    let mut points: Vec<Point<i64>> = Vec::new();
+fn get_points() -> Result<Vec<Point<i32>>, &'static str> {
+    let mut points: Vec<Point<i32>> = Vec::new();
     let mut index: u8 = 0;
 
     Ok( read_seed_file().lines().map(|x| {
         index += 1;
-        return parse_points(x, index);
+        parse_points(x, index)
     }).collect() )
 }
 
@@ -65,11 +65,13 @@ fn main() {
     let points = get_points().unwrap();
 
     let greatest_x = &points.iter().map(|c| { c.x } ).max().unwrap();
+    let smallest_x = &points.iter().map(|c| { c.x } ).min().unwrap();
     let greatest_y = &points.iter().map(|c| { c.y } ).max().unwrap();
+    let smallest_y = &points.iter().map(|c| { c.y } ).min().unwrap();
 
     let grid = Grid::new(
-        Point { x: 0 as usize, y: 0 as usize },
-        Point { x: *greatest_x as usize, y: *greatest_y as usize },
+        Point { x: (*smallest_x - 1), y: (*smallest_y - 1) },
+        Point { x: (*greatest_x + 1), y: (*greatest_y + 1) },
     );
 
     println!("{:?}", grid);
